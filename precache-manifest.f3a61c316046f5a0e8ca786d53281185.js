a55ac01cae190cd92513e47e032188d2d75446c2
Stanislav <stasizon@gmail.com>
2019-04-21 12:17:18 +0300

    Deploy on gh pages

---
+++ precache-manifest.f3a61c316046f5a0e8ca786d53281185.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2783410182ed69a621ff2dd0b9cd4fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6c51ddb2383a75b9fef",
+    "url": "/static/css/2.11a187e6.chunk.css"
+  },
+  {
+    "revision": "ae007e72b47ba06f46c5",
+    "url": "/static/css/main.ad4b894d.chunk.css"
+  },
+  {
+    "revision": "a6c51ddb2383a75b9fef",
+    "url": "/static/js/2.5a6ff99d.chunk.js"
+  },
+  {
+    "revision": "300a0a01a49a6ce568c9b658dbd7888d",
+    "url": "/static/js/2.5a6ff99d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae007e72b47ba06f46c5",
+    "url": "/static/js/main.cb1aa764.chunk.js"
+  },
+  {
+    "revision": "36721c1d80b91859afa9",
+    "url": "/static/js/runtime-main.a5593d4f.js"
+  }
+]);
